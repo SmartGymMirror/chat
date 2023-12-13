cf3b9534da7fdaa7c85398da2dddaacbd4adf3e1
albabernal03 <ALBABERO@MYUAX.COM>
2023-12-13 23:43:38 +0100

    c

--- asistente.py
+++ asistente.py
@@ -1,2 +1,15 @@
 import speech_recognition as sr
 import pyttsx3
+
+
+listener = sr.Recognizer()
+
+try:
+    with sr.Microphone() as source:
+        print('Escuchando...')
+        voice = listener.listen(source)
+        rec = listener.recognize_google(voice)
+        print(rec)
+
+except:
+    pass
